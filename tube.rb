e2b3df162198a9e07661ee678aa126a1e04282e5
macournoyer <macournoyer@gmail.com>
2013-06-21 13:36:14 -0400

    Implement converting the request to Rack.

--- tube.rb
+++ tube.rb
@@ -1,22 +1,25 @@
 require "socket"
 require "http/parser"
+require "stringio"
 
 class Tube
-  def initialize(port)
+  def initialize(port, app)
     @server = TCPServer.new(port)
+    @app = app
   end
 
   def start
     loop do
       socket = @server.accept
-      connection = Connection.new(socket)
+      connection = Connection.new(socket, @app)
       connection.process
     end
   end
 
   class Connection
-    def initialize(socket)
+    def initialize(socket, app)
       @socket = socket
+      @app = app
       @parser = Http::Parser.new(self)
     end
 
@@ -32,10 +35,22 @@ class Tube
       puts "  " + @parser.headers.inspect
       puts
 
-      send_response      
+      env = {}
+      @parser.headers.each_pair do |name, value|
+        # User-Agent => HTTP_USER_AGENT
+        name = "HTTP_" + name.upcase.tr("-", "_")
+        env[name] = value
+      end
+      env["PATH_INFO"] = @parser.request_path
+      env["REQUEST_METHOD"] = @parser.http_method
+      env["rack.input"] = StringIO.new
+
+      send_response env
     end
 
-    def send_response
+    def send_response(env)
+      status, headers, body = @app.call(env)
+
       @socket.write "HTTP/1.1 200 OK\r\n"
       @socket.write "\r\n"
       @socket.write "hello\n"
@@ -49,6 +64,18 @@ class Tube
   end
 end
 
-server = Tube.new(3000)
+class App
+  def call(env)
+    message = "Hello from the tube.\n"
+    [
+      200,
+      { 'Content-Type' => 'text/plain', 'Content-Length' => message.size.to_s },
+      [message]
+    ]
+  end
+end
+
+app = App.new
+server = Tube.new(3000, app)
 puts "Plugging tube into port 3000"
 server.start
