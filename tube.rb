d2c365caaa8677f5f6656bf82e9b988537119cda
macournoyer <macournoyer@gmail.com>
2013-06-21 13:26:33 -0400

    Parse the request.

--- tube.rb
+++ tube.rb
@@ -1,4 +1,5 @@
 require "socket"
+require "http/parser"
 
 class Tube
   def initialize(port)
@@ -8,15 +9,42 @@ class Tube
   def start
     loop do
       socket = @server.accept
+      connection = Connection.new(socket)
+      connection.process
+    end
+  end
 
-      data = socket.readpartial(1024)
-      puts data
+  class Connection
+    def initialize(socket)
+      @socket = socket
+      @parser = Http::Parser.new(self)
+    end
 
-      socket.write "HTTP/1.1 200 OK\r\n"
-      socket.write "\r\n"
-      socket.write "hello\n"
+    def process
+      until @socket.closed? || @socket.eof?
+        data = @socket.readpartial(1024)
+        @parser << data
+      end
+    end
 
-      socket.close
+    def on_message_complete
+      puts "#{@parser.http_method} #{@parser.request_path}"
+      puts "  " + @parser.headers.inspect
+      puts
+
+      send_response      
+    end
+
+    def send_response
+      @socket.write "HTTP/1.1 200 OK\r\n"
+      @socket.write "\r\n"
+      @socket.write "hello\n"
+
+      close
+    end
+
+    def close
+      @socket.close
     end
   end
 end
